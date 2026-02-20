ee34d2bfb9285182dc1677f96b5b36718ac396f0
seongeun040624 <seongeun040624@gmail.com>
2026-02-20 10:01:54 +0900

    2-1

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBS4Q8xi8NJyHRKZdH-9tFi0TzldywwauE",
-  authDomain: "mytrip-67e22.firebaseapp.com",
-  //databaseURL: "https://mytrip-67e22-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mytrip-67e22",
-  storageBucket: "mytrip-67e22.firebasestorage.app",
-  messagingSenderId: "538485328621",
-  appId: "1:538485328621:web:362d929e754427d10f1ae4",
-  measurementId: "G-PB226XSZCR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
